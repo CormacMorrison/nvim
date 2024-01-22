fd82391a6c7a70e8f8e5d51db60632ecda2403cf
Cormac Morrison <z5477834@ad.unsw.edu.au>
2024-01-23 01:55:18 +1100

    linting, formatting, autopairs, harpoon updates

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,4 +1,2 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set('n', '<C-t>', '<Cmd>tabnew<Cr>')
-vim.keymap.set('n', '<C-Tab>', '<Cmd>tabnext<Cr>')
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,88 +1,139 @@
 return {
-    {
-      'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-          dependencies = {
-		  -- LSP Support
-		  'neovim/nvim-lspconfig',
-		  'williamboman/mason.nvim',
-		  'williamboman/mason-lspconfig.nvim',
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		dependencies = {
+			-- LSP Support
+			"neovim/nvim-lspconfig",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
 
-		  -- Autocompletion
-		  'hrsh7th/nvim-cmp',
-		  'hrsh7th/cmp-buffer',
-		  'hrsh7th/cmp-path',
-		  'saadparwaiz1/cmp_luasnip',
-		  'hrsh7th/cmp-nvim-lsp',
-		  'hrsh7th/cmp-nvim-lua',
+			-- Autocompletion
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua",
 
-		  -- Snippets
-		  'L3MON4D3/LuaSnip',
-		  'rafamadriz/friendly-snippets',
-	  },
-    config = function()
-      local lsp = require("lsp-zero")
+			-- Snippets
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+		},
+		config = function()
+			local lsp = require("lsp-zero")
 
-      lsp.preset("recommended")
-      
-      lsp.ensure_installed({
-        'tsserver',
-        'lua_ls',
-        'rust_analyzer',
-        'clangd',
-      })
-      
-      -- Fix Undefined global 'vim'
-      lsp.nvim_workspace()
-      
-      
-      local cmp = require('cmp')
-      local cmp_select = {behavior = cmp.SelectBehavior.Select}
-      local cmp_mappings = lsp.defaults.cmp_mappings({
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-      })
-      
-      cmp_mappings['<Tab>'] = nil
-      cmp_mappings['<S-Tab>'] = nil
-      
-      lsp.setup_nvim_cmp({
-        mapping = cmp_mappings
-      })
-      
-      lsp.set_preferences({
-          suggest_lsp_servers = false,
-          sign_icons = {
-              error = 'E',
-              warn = 'W',
-              hint = 'H',
-              info = 'I'
-          }
-      })
-      
-      lsp.on_attach(function(client, bufnr)
-        local opts = {buffer = bufnr, remap = false}
-      
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-      end)
-      
-      lsp.setup()
-      
-      vim.diagnostic.config({
-          virtual_text = true
-      })
+			lsp.preset("recommended")
+			local mason = require("mason")
+			local mason_tool_installer = require("mason-tool-installer")
+			local mason_lspconfig = require("mason-lspconfig")
 
-    end
-    }    
+			mason.setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+
+			mason_lspconfig.setup({
+				-- Replace the language servers listed here
+				-- with the ones you want to install
+				ensure_installed = {
+					"tsserver",
+					"rust_analyzer",
+					"clangd",
+					"lua_ls",
+				},
+				handlers = {
+					lsp.default_setup,
+				},
+				automatic_installation = true,
+			})
+
+			mason_tool_installer.setup({
+				ensure_installed = {
+					"prettier", -- prettier formatter
+					"stylua", -- lua formatter
+					"isort", -- python formatter
+					"black", -- python formatter
+					"pylint", -- python linter
+					"eslint_d", -- js linter
+					"clang-format", -- c/cpp formatter
+				},
+			})
+			-- Fix Undefined global 'vim'
+			lsp.nvim_workspace()
+
+			local cmp = require("cmp")
+			local cmp_select = { behavior = cmp.SelectBehavior.Select }
+			local cmp_mappings = lsp.defaults.cmp_mappings({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			})
+
+			cmp_mappings["<Tab>"] = nil
+			cmp_mappings["<S-Tab>"] = nil
+
+			lsp.setup_nvim_cmp({
+				mapping = cmp_mappings,
+			})
+
+			lsp.set_preferences({
+				suggest_lsp_servers = false,
+				sign_icons = {
+					error = "E",
+					warn = "W",
+					hint = "H",
+					info = "I",
+				},
+			})
+
+			lsp.on_attach(function(client, bufnr)
+				local opts = { buffer = bufnr, remap = false }
+
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
+				end, opts)
+				vim.keymap.set("n", "K", function()
+					vim.lsp.buf.hover()
+				end, opts)
+				vim.keymap.set("n", "<leader>vws", function()
+					vim.lsp.buf.workspace_symbol()
+				end, opts)
+				vim.keymap.set("n", "<leader>vd", function()
+					vim.diagnostic.open_float()
+				end, opts)
+				vim.keymap.set("n", "[d", function()
+					vim.diagnostic.goto_next()
+				end, opts)
+				vim.keymap.set("n", "]d", function()
+					vim.diagnostic.goto_prev()
+				end, opts)
+				vim.keymap.set("n", "<leader>vca", function()
+					vim.lsp.buf.code_action()
+				end, opts)
+				vim.keymap.set("n", "<leader>vrr", function()
+					vim.lsp.buf.references()
+				end, opts)
+				vim.keymap.set("n", "<leader>vrn", function()
+					vim.lsp.buf.rename()
+				end, opts)
+				vim.keymap.set("i", "<C-h>", function()
+					vim.lsp.buf.signature_help()
+				end, opts)
+			end)
+
+			lsp.setup()
+
+			vim.diagnostic.config({
+				virtual_text = true,
+			})
+		end,
+	},
 }
